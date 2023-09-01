6549ce0e1ca504b0c7cbe559eac980081566a473
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-09-01 17:18:44 +0600

    modal created

---
+++ font-uploader/src/components/FontGroup.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const FontGroup = ({
+  fontList,
+  selectedFonts,
+  groupName,
+  onGroupNameChange,
+  onFontSelection,
+  onFontDeselection,
+  onSaveGroup,
+  onCancel,
+}) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white p-4 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-semibold mb-4">Create Font Group</h2>
+        <label className="block mb-2">
+          Group Name:
+          <input
+            type="text"
+            value={groupName}
+            onChange={onGroupNameChange}
+            className="block w-full border-gray-300 rounded-md p-2"
+            placeholder="Enter group name"
+          />
+        </label>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Selected Fonts</h3>
+          {selectedFonts.map((font) => (
+            <div key={font} className="flex items-center justify-between py-1">
+              <span>{font}</span>
+              <button
+                onClick={() => onFontDeselection(font)}
+                className="text-red-500 hover:text-red-600"
+              >
+                Deselect
+              </button>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Remaining Fonts</h3>
+          {fontList.map((font) => (
+            <div key={font} className="flex items-center justify-between py-1">
+              <span>{font}</span>
+              <button
+                onClick={() => onFontSelection(font)}
+                className="text-blue-500 hover:text-blue-600"
+              >
+                Select
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={onSaveGroup}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 mr-2"
+          >
+            Save Group
+          </button>
+          <button
+            onClick={onCancel}
+            className="bg-gray-300 text-gray-600 px-4 py-2 rounded-md hover:bg-gray-400"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FontGroup;
--- font-uploader/src/components/FontList.jsx
+++ font-uploader/src/components/FontList.jsx
@@ -1,56 +1,104 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
+import FontGroup from "./FontGroup";
 
 const FontList = () => {
   const [fontNames, setFontNames] = useState([]);
-
+  const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
+  const [groupName, setGroupName] = useState("");
+  const [selectedFonts, setSelectedFonts] = useState([]);
 
   useEffect(() => {
     // Fetch the list of font names from the backend
     fetch("http://localhost/font-uploader-server/upload-fonts.php")
-      .then(response => response.json())
-      .then(data => {
-        setFontNames(data); // Assuming data is an array of font names
+      .then((response) => response.json())
+      .then((data) => {
+        setFontNames(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching font names:", error);
       });
-  }, [fontNames]);
+  }, []);
 
   const columns = [
     {
       name: "Font Name",
       selector: "name",
       sortable: true,
-      format: row => row.name.replace(".ttf", "")
+      format: (row) => row.name.replace(".ttf", ""),
     },
     {
       name: "Font Preview",
       selector: "name",
-      cell: row => <div style={{ fontFamily: row.name.replace(".ttf", "") }}>{row.name.replace(".ttf", "")}</div>
+      cell: (row) => (
+        <div style={{ fontFamily: row.name.replace(".ttf", "") }}>
+          {row.name.replace(".ttf", "")}
+        </div>
+      ),
     },
     {
       name: "Action",
-      cell: row => <button onClick={() => handleDelete(row.name)}>Delete</button>
-    }
+      cell: (row) => (
+        <button onClick={() => handleDelete(row.name)}>Delete</button>
+      ),
+    },
   ];
 
-  const handleDelete = fontName => {
+  const handleDelete = (fontName) => {
     // Send a DELETE request to the backend to delete the font
-    fetch(`http://localhost/font-uploader-server/upload-fonts.php?fontName=${encodeURIComponent(fontName)}`, {
-      method: "DELETE",
-    })
-      .then(response => {
+    fetch(
+      `http://localhost/font-uploader-server/upload-fonts.php?fontName=${encodeURIComponent(
+        fontName
+      )}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => {
         if (response.ok) {
           // Font deleted successfully, remove it from the fontNames state
-          setFontNames(prevFontNames => prevFontNames.filter(name => name !== fontName));
+          setFontNames((prevFontNames) =>
+            prevFontNames.filter((name) => name !== fontName)
+          );
         } else {
           console.error("Failed to delete font:", response.status);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error deleting font:", error);
       });
+  };
+
+  const toggleCreateGroupModal = () => {
+    setShowCreateGroupModal(!showCreateGroupModal);
+    setGroupName(""); // Clear the group name input when the modal is toggled
+    setSelectedFonts([]); // Clear selected fonts when the modal is toggled
+  };
+
+  const handleFontSelection = (fontName) => {
+    setSelectedFonts((prevSelectedFonts) => [
+      ...prevSelectedFonts,
+      fontName,
+    ]);
+  };
+
+  const handleFontDeselection = (fontName) => {
+    setSelectedFonts((prevSelectedFonts) =>
+      prevSelectedFonts.filter((font) => font !== fontName)
+    );
+  };
+
+  const handleSaveGroup = () => {
+    // Create a new font group object with the group name and selected fonts
+    const newFontGroup = {
+      name: groupName,
+      fonts: selectedFonts,
+    };
+
+    // TODO: Send the newFontGroup to your backend or store it as needed
+
+    // Close the Create Group modal
+    toggleCreateGroupModal();
   };
 
   return (
@@ -58,12 +106,28 @@ const FontList = () => {
       <h2>Font List</h2>
       <DataTable
         columns={columns}
-        data={fontNames.map(name => ({ name }))} // Format data as array of objects
+        data={fontNames.map((name) => ({ name }))}
         pagination
         paginationPerPage={5}
         paginationRowsPerPageOptions={[10, 20, 30]}
       />
-     
+      <div className="text-right mt-10">
+        <button onClick={toggleCreateGroupModal}>Create Group</button>
+      </div>
+
+      {/* Render the Create Group modal */}
+      {showCreateGroupModal && (
+        <FontGroup
+          fontList={fontNames}
+          selectedFonts={selectedFonts}
+          groupName={groupName}
+          onGroupNameChange={(e) => setGroupName(e.target.value)}
+          onFontSelection={handleFontSelection}
+          onFontDeselection={handleFontDeselection}
+          onSaveGroup={handleSaveGroup}
+          onCancel={toggleCreateGroupModal}
+        />
+      )}
     </div>
   );
 };
